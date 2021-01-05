a38d043e3cedbde5952e1be81e3477a7b0a20e95
cbdyzj <cbdyzj@jianzhao.org>
2021-01-05 21:38:03 +0800

    Init

---
+++ webpack.config.cjs
@@ -0,0 +1,67 @@
+const { resolve } = require('path')
+const { ESBuildPlugin } = require('esbuild-loader')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const templatePlugin = require('./template.plugin.cjs')
+
+const config = {
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }, {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'esbuild-loader',
+                    options: {
+                        loader: 'tsx',
+                        target: 'es2020'
+                    }
+                }],
+                exclude: /node_modules/,
+            }, {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: [{ loader: 'file-loader', options: { outputPath: 'assets' } }],
+            }, {
+                test: /\.(css|less)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: { modules: true } },
+                    { loader: 'less-loader' },
+                ],
+                exclude: /node_modules/
+            }, {
+                test: /\.(css|less)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' },
+                ],
+                include: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new ESBuildPlugin(),
+        new VueLoaderPlugin(),
+        ...templatePlugin,
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            automaticNameDelimiter: '_',
+        },
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+}
+
+module.exports = config
